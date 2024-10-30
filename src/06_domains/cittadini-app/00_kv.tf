0959189e7caf1a5c1313405202dac0bde50dedc6
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-10-30 16:24:41 +0100

    updated kv

--- src/06_domains/cittadini-app/00_kv.tf
+++ src/06_domains/cittadini-app/00_kv.tf
@@ -16,3 +16,14 @@ data "azurerm_key_vault_secret" "argocd_admin_password" {
   name         = "argocd-admin-password"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+data "azurerm_key_vault" "kv" {
+  name                = "${local.product}-${var.domain}-kv"
+  resource_group_name = "${local.product}-${var.domain}-sec-rg"
+}
+
+data "azurerm_key_vault_secret" "alert-slack-arc" {
+  count = var.arc_alert_enabled ? 1 : 0
+  name         = "alert-arc-notification-slack"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
