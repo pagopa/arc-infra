c13b2644eb01607cabf4cb733ddecaaea9832f47
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-10-30 16:40:29 +0100

    variable location

--- src/06_domains/cittadini-app/99_variables.tf
+++ src/06_domains/cittadini-app/99_variables.tf
@@ -97,13 +97,3 @@ variable "arc_alert_enabled" {
   type    = bool
   default = false
 }
-
-variable "log_analytics_workspace_name" {
-  type        = string
-  description = "Specifies the name of the Log Analytics Workspace."
-}
-
-variable "log_analytics_workspace_resource_group_name" {
-  type        = string
-  description = "The name of the resource group in which the Log Analytics workspace is located in."
-}
